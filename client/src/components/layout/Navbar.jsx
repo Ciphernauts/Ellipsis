d0c9403edc3a1f31d30cfafbde18d5eed37cb030
mariah <mariahkhalifa05@gmail.com>
2025-01-21 23:11:36 +0400

    create navbar

---
+++ client/src/components/layout/Navbar.jsx
@@ -0,0 +1,27 @@
+import styles from './Navbar.module.css';
+import Logo from '../icons/Logo';
+import Button from '../Button';
+import { Link } from 'react-router-dom';
+
+export default function LandingPage() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.brand}>
+        <Logo />
+        <p className={styles.logoName}>Ellipsis</p>
+      </div>
+      <div className={styles.navlinks}>
+        <Link to="/services" className={styles.link}>
+          Services
+        </Link>
+        <Link to="/about" className={styles.link}>
+          About
+        </Link>
+        <Link to="/contact" className={styles.link}>
+          Contact
+        </Link>
+        <Button color="primary" fill={false} text="Log In" to="/login" />
+      </div>
+    </div>
+  );
+}
