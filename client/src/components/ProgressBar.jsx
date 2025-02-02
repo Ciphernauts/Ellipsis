984d2e1efc6d442ecf2738f9afa6fd1154069d5a
Mariah0_0 <mariahkhalifa05@gmail.com>
2025-02-02 08:52:55 +0400

    make progress bar component

---
+++ client/src/components/ProgressBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./ProgressBar.module.css";
+
+export default function ProgressBar({
+  progress = 100,
+  width = "100px",
+  color = "var(--primary)",
+  className = "",
+}) {
+  return (
+    <div
+      className={`${styles.progressBar} ${className}`}
+      style={{ width: width }}
+    >
+      <div
+        className={styles.progressBarFill}
+        style={{ width: `${progress}%`, backgroundColor: color }}
+      ></div>
+    </div>
+  );
+}
