785457dfc4dfa8d7c98a785a97c2a6a45da87655
Mariah0_0 <mariahkhalifa05@gmail.com>
2025-02-02 08:53:53 +0400

    make dashboard safety score card

---
+++ client/src/components/dashboard/SafetyScoreCard.jsx
@@ -0,0 +1,115 @@
+import React, { PureComponent } from "react";
+import Percentage from "../Percentage";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import styles from "./SafetyScoreCard.module.css";
+import ProgressBar from "../ProgressBar";
+
+export default function SafetyScoreCard() {
+  // Placeholder data
+  const data = {
+    ppe: {
+      helmet: 97.3,
+      vest: 74.9,
+      footwear: 86.9,
+      gloves: 94.7,
+    },
+    fall: {
+      scaffolding: 77.0,
+      guardrails: 51.8,
+      harness: 87.5,
+    },
+  };
+
+  // Helper function to calculate the average of an object
+  const calculateAverage = (values) => {
+    const sum = Object.values(values).reduce((acc, value) => acc + value, 0);
+    return sum / Object.values(values).length;
+  };
+
+  // Calculate PPE and Fall averages
+  const ppeAvg = calculateAverage(data.ppe);
+  const fallAvg = calculateAverage(data.fall);
+
+  // Calculate the total average
+  const totalAvg = ((ppeAvg + fallAvg) / 2).toFixed(1);
+
+  // Calculate weighted averages for PPE and Fall
+  const ppeWeighted = ((ppeAvg / (ppeAvg + fallAvg)) * totalAvg).toFixed(1);
+  const fallWeighted = ((fallAvg / (ppeAvg + fallAvg)) * totalAvg).toFixed(1);
+
+  const chartData = [
+    { name: "total", value: 100 - totalAvg },
+    { name: "fall", value: parseFloat(fallWeighted) },
+    { name: "ppe", value: parseFloat(ppeWeighted) },
+  ];
+
+  const COLORS = [
+    "var(--background-color)",
+    "var(--secondary)",
+    "var(--primary)",
+  ];
+
+  return (
+    <div className={styles.card}>
+      <h2>Safety Score Overview</h2>
+      <div className={styles.content}>
+        <div className={styles.overall}>
+          <Percentage
+            number={totalAvg}
+            label="Overall avg"
+            className={styles.percentage}
+          />
+          <PieChart width={160} height={160} title="Safety Score" fill="black">
+            <Pie
+              data={chartData}
+              innerRadius={60}
+              outerRadius={80}
+              cornerRadius={10}
+              paddingAngle={-15}
+              startAngle={0}
+              fill="black"
+              stroke="none"
+              dataKey="value"
+            >
+              {chartData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                  style={{ outline: "none" }}
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </div>
+        <div className={styles.breakdown}>
+          <div className={styles.section}>
+            <Percentage
+              number={ppeWeighted}
+              label="PPE Detection"
+              numberSize={22}
+              symbolSize={16}
+              className={styles.percentage}
+            />
+            <ProgressBar
+              progress={ppeWeighted}
+              className={styles.progressBar}
+            />
+          </div>
+          <div className={styles.section}>
+            <Percentage
+              number={fallWeighted}
+              label="Fall Protection"
+              numberSize={22}
+              symbolSize={16}
+              className={styles.percentage}
+            />
+            <ProgressBar
+              progress={fallWeighted}
+              className={styles.progressBar}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,3 +1,16 @@
+import SafetyScoreCard from "../components/dashboard/SafetyScoreCard";
+import SafetyScoreTrends from "../components/dashboard/SafetyScoreTrends";
+import styles from "./Dashboard.module.css";
+
 export default function Dashboard() {
-  return <></>;
+  return (
+    <div className={styles.dashboard}>
+      <h1>Dashboard</h1>
+      <main>
+        <div className={styles.row}>
+          <SafetyScoreCard />
+        </div>
+      </main>
+    </div>
+  );
 }
