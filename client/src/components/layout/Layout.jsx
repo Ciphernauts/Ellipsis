443e04ee24a723afb0cabacc99d58b217316f346
Mariah0_0 <mariahkhalifa05@gmail.com>
2025-02-20 18:33:22 +0400

    add settings and logout options in navpane, and make crossIcon a separate component

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -9,6 +9,7 @@ import SessionsInfoPane from './infoPanes/SessionsInfoPane';
 import IncidentsInfoPane from './infoPanes/IncidentsInfoPane';
 import ConstructionSitesInfoPane from './infoPanes/ConstructionSitesInfoPane';
 import styles from './Layout.module.css';
+import CrossIcon from '../icons/CrossIcon';
 
 const Layout = () => {
   const location = useLocation();
@@ -103,20 +104,7 @@ const Layout = () => {
                 setPaneData(null);
               }}
             >
-              <svg
-                width='27px'
-                height='27px'
-                viewBox='0 0 24 24'
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-              >
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M19.207 6.207a1 1 0 0 0-1.414-1.414L12 10.586 6.207 4.793a1 1 0 0 0-1.414 1.414L10.586 12l-5.793 5.793a1 1 0 1 0 1.414 1.414L12 13.414l5.793 5.793a1 1 0 0 0 1.414-1.414L13.414 12l5.793-5.793z'
-                  fill='var(--dark)'
-                />
-              </svg>
+              <CrossIcon size='25px' />
             </span>
           )}
           {getRightPaneContent()}
