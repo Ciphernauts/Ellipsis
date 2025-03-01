5c5f148f0e309afc4300d031da6df538e2ac8b18
HadiyyaMattummathodi <hsshh999@uowmail.edu.au>
2025-03-01 23:28:00 +0400

    Fixing fetching routes to match frontend

--- server/Routes/ConstructionSitesRoutes.js
+++ server/Routes/ConstructionSitesRoutes.js
@@ -4,11 +4,10 @@ const controller = require('../Controllers/ConstructionSites.js');
 const router = Router();
 
 router.get('/construction-sites', controller.getAllConstructionSites);
-router.get('/construction-sites/:site_id', controller.getConstructionSiteById);
 
 router.put('/construction-sites/:site_id', controller.updateStatus);
 
-router.post('/construction-sites/:name', controller.addConstructionSite);
+router.post('/construction-sites/:name', controller.addConstructionSite);//default active
 
 router.delete('/construction-sites/:site_id', controller.deleteConstructionSite);
 
