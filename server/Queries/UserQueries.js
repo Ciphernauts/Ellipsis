0ebc170aeabe94a0ceb08eaed2898af9c1b6ac2c
Aryan Malhotra <aryanm0604@gmail.com>
2025-03-02 12:10:37 +0400

    Fixed LoginRegister Functionality (Remove the commented code when it is working)

--- server/Queries/UserQueries.js
+++ server/Queries/UserQueries.js
@@ -3,8 +3,8 @@ const getUserById = "SELECT * FROM users WHERE uid = $1";
 const checkEmailExists = "SELECT * from users WHERE uemail = $1";
 const removeUser = "DELETE FROM users WHERE uid = $1";
 const updateUser = "UPDATE users SET username = $1 WHERE uid = $2";
-const getUserByEmail = "SELECT * FROM users WHERE uemail = $1";
-const addUser = "INSERT INTO users (username, uemail, password, role, last_signin) VALUES ($1, $2, $3, 'standard', NOW()) RETURNING *";
+const getUserByEmail = "SELECT * FROM users WHERE LOWER(uemail) = LOWER($1)";
+const addUser = "INSERT INTO users (uemail, username, password, role, last_signin) VALUES ($1, $2, $3,'standard', NOW())";
 module.exports = {
     getUsers,
     checkEmailExists,
