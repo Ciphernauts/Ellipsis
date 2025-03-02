495d3c7937fffddef0907fa666cc2e671030e701
HadiyyaMattummathodi <hsshh999@uowmail.edu.au>
2025-03-02 23:22:25 +0400

    Add sql to get all incident trends, breakdown,, alert metrics

--- server/Routes/IncidentTrendsRoutes.js
+++ server/Routes/IncidentTrendsRoutes.js
@@ -3,7 +3,6 @@ const controller = require('../Controllers/IncidentTrends.js');
 
 const router = Router();
 
-// router.get('/incidents/trends-trends', controller.getAllIncidentTrends);
 router.get('/incidents/incident-trends', controller.getAllIncidentTrends); //updated what i think is a typo?
 
 module.exports = router;
