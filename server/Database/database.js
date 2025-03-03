adb0d55aae442aa1f2c3733920650c67d71bee1f
HadiyyaMattummathodi <hsshh999@uowmail.edu.au>
2025-03-03 22:25:57 +0400

    Remove unused fsevents module and update various package configurations, change db name to postgres

--- server/Database/database.js
+++ server/Database/database.js
@@ -4,7 +4,7 @@ const pool = new Pool ({
     //connectionString: 'postgres://postgres:root@localhost:5432/postgres'
     user:"postgres",
     host:"localhost",
-    database:"ObjectDetection",
+    database:"localhost",
     password:"root",
     port:5432
 });
