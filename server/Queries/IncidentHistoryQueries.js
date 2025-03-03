64dcfd02efa8e8656584fa098dae4abd28052adc
HadiyyaMattummathodi <hsshh999@uowmail.edu.au>
2025-03-03 21:46:59 +0400

    Refactor incident history controller and queries for improved error handling and response structure

--- server/Queries/IncidentHistoryQueries.js
+++ server/Queries/IncidentHistoryQueries.js
@@ -1,33 +1,64 @@
-const getAllIncidentHistory = `
+const getAllIncidentHistory = `SELECT
+JSON_BUILD_OBJECT(
+  'incidents', (
     SELECT
-        i.incident_id AS "id",
-        i.session_id,
-        i.incident_time AS "time",
-        i.severity,
-        i.status,
-        i.category,
-        s.site_id,
-        cs.name AS "site",
-        s.mode,
-        c.name AS "device",
-        c.type AS "deviceType",
-        ss.image_url AS "snapshot"
-    FROM
-        incidents i
-    JOIN
-        sessions s ON i.session_id = s.session_id
-    JOIN
-        construction_sites cs ON s.site_id = cs.site_id
-    JOIN
-        cameras c ON s.camera_id = c.camera_id
-    LEFT JOIN
-        snapshots ss ON i.session_id = ss.session_id
-    ORDER BY
-        i.incident_time DESC;
-`;
+      JSON_AGG(
+        JSON_BUILD_OBJECT(
+          'id', i.incident_id,
+          'category', i.category,
+          'snapshot', (
+            SELECT
+              image_url
+            FROM snapshots
+            WHERE
+              session_id = i.session_id
+            ORDER BY
+              "timestamp" DESC
+            LIMIT 1
+          ),
+          'site', cs.name,
+          'time', i.incident_time,
+          'status', i.status,
+          'isCritical', (
+            CASE WHEN i.severity = 'Critical'
+            THEN TRUE
+            ELSE FALSE
+            END
+          ),
+          'sessionId', i.session_id,
+          'mode', s.mode,
+          'device', JSON_BUILD_OBJECT(
+            'name', c.name,
+            'type', c.type
+          )
+        )
+      )
+    FROM incidents AS i
+    JOIN sessions AS s
+      ON i.session_id = s.session_id
+    JOIN construction_sites AS cs
+      ON s.site_id = cs.site_id
+    JOIN cameras AS c
+      ON s.camera_id = c.camera_id
+  ),
+  'constructionSites', (
+    SELECT
+      JSON_AGG(name)
+    FROM construction_sites
+  )
+) AS response_data;`
 
-//add filters part al;so
+
+const updateIncidentStatus = `WITH UpdatedIncident AS (
+    UPDATE incidents
+    SET status = $2  -- $2 is the new status from the request
+    WHERE incident_id = $1  -- $1 is the incident ID from the request
+    RETURNING status  -- Return the updated status
+)
+SELECT JSON_BUILD_OBJECT('status', status)  -- Build a JSON object with the status
+FROM UpdatedIncident;`
 
 module.exports = {
     getAllIncidentHistory,
+    updateIncidentStatus
 };
