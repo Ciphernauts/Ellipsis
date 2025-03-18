45f83f80c29e857a29dd479eaee05f0897c3124e
Aryan Malhotra <aryanm0604@gmail.com>
2025-03-18 19:41:20 +0400

    Updating the database to the Render database

--- server/Database/database.js
+++ server/Database/database.js
@@ -1,3 +1,4 @@
+/*
 const { Pool } = require('pg');
 
 const host= process.env.PG_HOST;
@@ -10,6 +11,20 @@ const pool = new Pool ({
     database:"Ellipsis",
     password:"Post",
     port:5433
+});
+
+module.exports = pool;
+*/
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  //connectionString: 'postgresql://ellipsis_user:ffgJTH7dmIRWHgHcdFJLZBRUGR2Kc0Ak@dpg-cvaugf2n91rc739bco30-a/ellipsis'
+  user: "ellipsis_user",
+  host: "dpg-cvaugf2n91rc739bco30-a",
+  database: "ellipsis",
+  password: "ffgJTH7dmIRWHgHcdFJLZBRUGR2Kc0Ak",
+  port: 5432,
 });
 
 module.exports = pool;
